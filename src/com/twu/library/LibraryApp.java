7816d421d7f86937c6c5537108e431b219db74e3
cmoers <carolinemoers@gmail.com>
2015-04-22 12:38:19 -0300

    Refactor LibraryApp, reduce methods and reviem duplicate code

--- src/com/twu/library/LibraryApp.java
+++ src/com/twu/library/LibraryApp.java
@@ -19,81 +19,91 @@ public class LibraryApp {
         try{
             Message message = new Message();
             System.out.println(message.showWelcomeMessage());
-
-            while(keepRunning) {
-                Menu menu = new Menu();
-                String mainMenu = menu.mainMenu();
-                System.out.println(mainMenu);
-
-                Scanner input = new Scanner(System.in);
-                System.out.print(message.showInputDataMessage());
-                int optionChoose = input.nextInt();
-
-                switch (optionChoose) {
-                    case 1:
-                        showBooksLibrary();
-                        break;
-                    case 2:
-                        showBooksCheckout();
-                        break;
-                    case 3:
-                        showBooksToReturn();
-                        break;
-                    case 4:
-                        keepRunning = false;
-                        System.out.close();
-                        break;
-                    default:
-                        System.out.println("Select a valid option!");
-                        break;
-                }
-            }
+            optionsApp(message);
         }catch (Exception ex){
             System.out.println("Something got wrong, the applicattion will close. Error: " + ex.getMessage());
         }
+    }
+
+    private static void optionsApp(Message message){
+        while(keepRunning) {
+            Menu menu = new Menu();
+            String mainMenu = menu.mainMenu();
+            System.out.println(mainMenu);
+
+            Integer optionChoose = getInputData(message.showInputDataMessage());
+
+            switch (optionChoose) {
+                case 1:
+                    showBooksLibrary();
+                    break;
+                case 2:
+                    showBooksToCheckout(message.showInputDataMessageCheckoutBook(), Books.getAvailableBooksToCheckout(books));
+                    break;
+                case 3:
+                    showBooksToReturn(message.showInputDataMessageReturnBook(), Books.getAvailableBooksToReturn(books));
+                    break;
+                case 4:
+                    keepRunning = false;
+                    System.out.close();
+                    break;
+                default:
+                    System.out.println("Select a valid option!");
+                    break;
+            }
+        }
+    }
+
+    private static Integer getInputData(String message){
+
+            Scanner input = new Scanner(System.in);
+            System.out.print(message);
+            return input.nextInt();
+
     }
 
     private static void showBooksLibrary(){
         showInfoBooks(Books.getAvailableBooksToCheckout(books));
     }
 
-    private static void showBooksCheckout(){
-        List<Book> availableBooks = Books.getAvailableBooksToCheckout(books);
-        showInfoBooks(Books.getAvailableBooksToCheckout(availableBooks));
-
-        Message message = new Message();
-        Scanner input = new Scanner(System.in);
-        System.out.print(message.showInputDataMessageCheckoutBook());
-        int code = input.nextInt();
-        Book book = new Book();
-
-        String resultCheckout = book.checkoutBookFor(code, availableBooks);
-        System.out.println(resultCheckout);
+    private static void showBooksToCheckout(String message, List<Book> books){
+        try{
+            showInfoBooks(books);
+            Integer code = getInputData(message);
+            String resultCheckout = Book.checkoutBookFor(code, books);
+            System.out.println(resultCheckout);
+        } catch (java.util.InputMismatchException ex) {
+            System.out.println("Invalid input, needs to be a number.");
+        }
     }
 
-    private static void showBooksToReturn(){
-        showInfoBooks(Books.getAvailableBooksToReturn(books));
-
-        Message message = new Message();
-        Scanner input = new Scanner(System.in);
-        System.out.print(message.showInputDataMessageReturnBook());
-        int code = input.nextInt();
-        Book book = new Book();
-
-        String resultCheckout = book.returnBookToLibraryFor(code, books);
-        System.out.println(resultCheckout);
+    private static void showBooksToReturn(String message, List<Book> books){
+        try{
+            showInfoBooks(books);
+            Integer code = getInputData(message);
+            String resultCheckout = Book.returnBookToLibraryFor(code, books);
+            System.out.println(resultCheckout);
+        } catch (java.util.InputMismatchException ex) {
+            System.out.println("Invalid input, needs to be a number.");
+        }
     }
 
     private static void showInfoBooks(List<Book> books){
-        String leftAlignFormat = "| %-4s | %-25s | %-25s | %-5s |%n";
+        if(!books.isEmpty()) {
+            String leftAlignFormat = "| %-4s | %-25s | %-25s | %-5s |%n";
 
-        System.out.format("+------+---------------------------+---------------------------+-------+%n");
-        System.out.printf("| Code | Title                     | Author                    | Year  |%n");
-        System.out.format("+------+---------------------------+---------------------------+-------+%n");
+            System.out.format("+------+---------------------------+---------------------------+-------+%n");
+            System.out.printf("| Code | Title                     | Author                    | Year  |%n");
+            System.out.format("+------+---------------------------+---------------------------+-------+%n");
 
-        for(Book book : books){
-            System.out.format(leftAlignFormat, book.getCode(), book.getTitle(), book.getAuthor(), book.getYearPublished());
+            for (Book book : books) {
+                System.out.format(leftAlignFormat, book.getCode(), book.getTitle(), book.getAuthor(), book.getYearPublished());
+            }
+            System.out.format("+------+---------------------------+---------------------------+-------+%n");
+        }else{
+            System.out.format("+------+---------------------------+---------------------------+-------+%n");
+            System.out.printf("|                    There is no available books                       |%n");
+            System.out.format("+------+---------------------------+---------------------------+-------+%n");
         }
-        System.out.format("+------+---------------------------+---------------------------+-------+%n");
     }
 }
